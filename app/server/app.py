80f0e95d2d5f20f476a993bbc17bee5d530cde63
howyay <tangh4@rpi.edu>
2024-11-10 10:55:22 -0500

    fix app.py

--- app/server/app.py
+++ app/server/app.py
@@ -1,19 +1,7 @@
 from fastapi import FastAPI
 from .routes.model_routes import router as model_router
 
-
-
-app = FastAPI()
-
-app.include_router(model_router, prefix="/model")
-
-@app.get("/", tags=["Root"])
-async def read_root():
-    return {"message": "Healthcheck!"}
-
 from fastapi.middleware.cors import CORSMiddleware
-from .routes.loc_routes import router as loc_router
-from .routes.img_routes import router as img_router
 
 app = FastAPI()
 
@@ -24,3 +12,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.include_router(model_router, prefix="/model")
+
+@app.get("/", tags=["Root"])
+async def read_root():
+    return {"message": "Healthcheck!"}
